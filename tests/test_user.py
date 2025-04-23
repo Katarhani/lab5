664fef9f7934c5a78b069dcb283cc3be9ff3dbef
Katarhani <Ianchikk.3@yandex.ru>
2025-04-23 13:26:57 +0300

    NewCommit

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,9 +1,7 @@
 from fastapi import status
-from fastapi.testclient import TestClient
 from src.main import app
 
 client = TestClient(app)
-BASE_URL = "/api/v1/user"  # Общий endpoint для всех тестов
 
 # Тестовые данные
 users = [
